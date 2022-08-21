da989ce71e26bed5f342bfbdcef7e02117a349bb
Juliano C L Santos <foxports@hotmail.com>
2022-08-21 01:02:55 -0300

    initials commit

---
+++ src/main/java/br/com/jcls/greetingapigateway/GreetingApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.jcls.greetingapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jcls/greetingapigateway/GreetingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jcls.greetingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
